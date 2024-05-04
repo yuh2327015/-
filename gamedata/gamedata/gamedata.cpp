84aa3292fba0b785251271a3f8640ca7c5b01f9f
Odangg <ss4188@naver.com>
2024-05-04 18:05:38 +0900

    0504 업데이트

--- gamedata/gamedata/gamedata.cpp
+++ gamedata/gamedata/gamedata.cpp
@@ -1,9 +1,14 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
 #include <string>
 #include <Windows.h>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
 
 #define COLOR_GREEN 10
 #define COLOR_RED 12
@@ -53,20 +58,30 @@ void saveGameData(int enhancements, int successes, int failures, int csvEnhancem
     }
 }
 
-
 int main() {
     std::srand(std::time(nullptr));
 
-    int enhancementLevel = 0;
+    int enhancementLevel = 0; // 현재 강화 레벨 변수
     int successes = 0;
     int failures = 0;
     int upgradetry = 1; // 구글 시트에 강화 횟수 더해주는 변수
     int maxEnhancements = 0;
     int csvEnhancements = 0;
-
+    int activeNum = 1; // 구글 시트 Apps Scripts 함수 제어 변수
+    bool gameon = true; // while 문 통제 변수
+    std::string username;
+    // 현재시간 저장하는 변수
+    auto now = std::chrono::system_clock::now();
+    // 시간 포인터를 시간 구조체로 변환
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    struct std::tm* ptm = std::localtime(&now_time);
+    // 날짜와 시간을 문자열로 변환하여 변수에 저장
+    std::ostringstream oss;
+    oss << std::put_time(ptm, "%Y.%m.%d %H시%M분%S초");
+    std::string starttime = oss.str();  // 시작 시간 저장 변수
     // 구글 시트 Apps Scripts url
     std::string url = "https://script.google.com/macros/s/AKfycbwcZtJomdqP7A4iDwyOpD4PuQVS5oKzF2mHw5n7jbwbJGKFHG1u95QbVR-EpkWR7oXR/exec";
-    // 커맨드로 보낼 데이터 입력
+    // 커맨드로 보낼 데이터 변수
     std::string data;
     std::string command;
     int successChance[] = { 95, 85, 75, 60, 50, 35, 20, 10, 5, 2 };
@@ -75,7 +90,13 @@ int main() {
     int choice;
     bool once; // 버퍼를 첫 1회 및 장비 파괴 후 다시 시작할때만 지우기 위해 만든 변수
 
-    while (true) {
+    std::cout << "유저명을 입력해주세요 : ";
+    std::cin >> username;
+    std::cout << "환영합니다. " << username << "님";
+    Sleep(1000);
+    system("cls");
+
+    while (gameon) {
         showMainMenu();
         once = true;
         std::cout << "선택하세요: ";
@@ -102,6 +123,12 @@ int main() {
                 }
                 std::cin.get();    // 엔터키 입력 대기
                 std::cout << "강화 중입니다...\n";
+                now = std::chrono::system_clock::now(); // 시간 갱신
+                now_time = std::chrono::system_clock::to_time_t(now);
+                ptm = std::localtime(&now_time);
+                std::ostringstream oss2;
+                oss2 << std::put_time(ptm, "%Y.%m.%d %H시%M분%S초");
+                std::string trytime = oss2.str();
                 Sleep(1000);
 
                 int result = std::rand() % 100;
@@ -158,7 +185,9 @@ int main() {
                     }
                 }
 
-                data = "upgradetry=" + std::to_string(upgradetry) + "&success=" + std::to_string(successes) + "&fail=" + std::to_string(failures);
+                // 커맨드로 보낼 데이터 입력
+                data = "upgradetry=" + std::to_string(upgradetry) + "&username="+ username + "&starttime="+ starttime + "&trytime=" 
+                    + trytime + "&enhancementLevel=" + std::to_string(enhancementLevel);
                 command = "curl -X POST -d \"" + data + "\" \"" + url + "\"";
                 // 보내고 이전내용 화면에서 지우기
                 system(command.c_str());
@@ -182,12 +211,24 @@ int main() {
             break;
         case 3:
             std::cout << "게임을 종료합니다.\n";
-            return 0;
+            gameon = false;
+            break;
         default:
             std::cout << "잘못된 선택입니다. 다시 선택하세요.\n";
             break;
         }
     }
+
+    now = std::chrono::system_clock::now();
+    now_time = std::chrono::system_clock::to_time_t(now);
+    ptm = std::localtime(&now_time);
+
+    std::ostringstream oss3;
+    oss3 << std::put_time(ptm, "%Y.%m.%d %H시%M분%S초");
+    std::string endtime = oss3.str();  // 시작 시간 저장 변수
+    data = "upgradetry=" + std::to_string(upgradetry) + "&username=" + username + "&starttime=" + starttime + "&endtime=" + endtime + "&activeNum=" + std::to_string(activeNum);
+    command = "curl -X POST -d \"" + data + "\" \"" + url + "\"";
+    system(command.c_str());
 
     return 0;
 }
